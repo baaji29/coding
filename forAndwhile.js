5c6a5ebad387b80100eac1514c26b0b89e837a83
Balaji <43308457+baaji29@users.noreply.github.com>
2024-04-08 02:39:34 +0000

    forAndwhile Execution

---
+++ forAndwhile.js
@@ -0,0 +1,41 @@
+const x = [1,2,3,4,5]
+
+// for ( i = 0; i ==5 ; ) {
+//     const element = x[i];
+//     console.log(element)
+
+    
+// }
+
+// while (i == 4) {
+//     console.log(x)
+// }
+
+let result = 0
+// let i = 0
+
+// while(result !== 6) {
+//    result= Math.floor(Math.random() * 10) + 1;
+//    console.log(result);
+// //    i++
+// }
+// for(;result!==6;)
+// {
+//     result= Math.floor(Math.random() * 10) + 1;
+//     console.log(result);
+// }
+
+let sum = 0;
+let num = 1;
+// while (sum < 100) {
+//     sum += num;
+//     num++;
+//     console.log("Current sum:", sum);
+// }
+
+
+for (; sum < 100 ;) {
+    sum += num;
+    num++
+    console.log("SUM",sum, num)    
+}
